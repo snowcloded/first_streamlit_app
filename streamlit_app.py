ef0e6c7693d81dc495f51c7f23d7998329669603
snowcloded <146838805+snowcloded@users.noreply.github.com>
2023-10-03 17:38:26 +0530

    Update streamlit_app.py
    text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My  Parets New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 &  blueberry oatmeal')
+streamlit.text('Kale, spinach & Rocket smoothie')
+streamlit.text('Hard-Boiled Free_Range Egg')
